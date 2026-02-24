0c3896bee843c6669175ae1b924ab307ac9eea54
vitoriadesouza <vitoriasouza.ifc@gmail.com>
2026-02-24 16:47:58 -0300

    FEAT: criação da model Categoria

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,7 @@
 Django admin customization.
 """
 
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -52,3 +53,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Categoria)
